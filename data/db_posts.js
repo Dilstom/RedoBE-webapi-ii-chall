431097777ed50efcfed7c0dde69ba1527c342692
Dil Sombayeva <dilyasom@gmail.com>
2019-07-26 16:45:57 -0700

    add put('/:id') req

--- data/db_posts.js
+++ data/db_posts.js
@@ -98,6 +98,32 @@ router.delete('/:id', (req, res) => {
   });
 });
 
+router.put('/:id', (req, res) => {
+ const id = req.params.id;
+ const body = req.body;
+
+ console.log(body);
+ if (!body.title || !body.contents) {
+  res
+   .status(400)
+   .json({ errorMessage: 'Please provide title and contents for the post.' });
+  return;
+ }
+ postsDB
+  .update(id, body)
+  .then(postID => {
+   if (postID) {
+    res.status(200).json(postID);
+   } else {
+    res
+     .status(404)
+     .json({ message: 'The post with the specified ID does not exist.' });
+   }
+  })
+  .catch(err => {
+   res
+    .status(500)
+    .json({ error: 'The post information could not be modified.' });
   });
 });
 
