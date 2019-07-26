9f5467d4e9caa42fb7484ee74d8f2c904cf34fdf
Dil Sombayeva <dilyasom@gmail.com>
2019-07-26 16:43:39 -0700

    add messages

--- data/db_posts.js
+++ data/db_posts.js
@@ -10,7 +10,9 @@ router.get('/', (req, res) => {
    res.status(200).json(posts);
   })
   .catch(err => {
-   res.status(500).json({ err });
+   res
+    .status(500)
+    .json({ error: 'The posts information could not be retrieved.' });
   });
 });
 
@@ -22,7 +24,17 @@ router.get('/:id', (req, res) => {
    if (post && post.length > 0) {
     res.status(200).json(post);
    } else {
-    res.status(404).json({ message: 'Post not found' });
+    res
+     .status(404)
+     .json({ message: 'The post with the specified ID does not exist.' });
+   }
+  })
+  .catch(err => {
+   res
+    .status(500)
+    .json({ error: 'The post information could not be retrieved.' });
+  });
+});
    }
   })
   .catch(err => {
